4b7b8f306c7005bfcf205b75553af960c244df60
Ali Raza <ali.raza@cybbernest.com>
2023-04-26 18:21:03 +0500

    translation keys and values modification of case list views

--- frontend/app/scripts/services/ui/CaseTabsSrv.js
+++ frontend/app/scripts/services/ui/CaseTabsSrv.js
@@ -1,91 +1,91 @@
 (function() {
-    'use strict';
-    angular.module('theHiveServices').service('CaseTabsSrv', function(i18n) {
+	'use strict';
+	angular.module('theHiveServices').service('CaseTabsSrv', function(i18n) {
 
-        var tabs = {
-            'details': {
-                name: 'details',
-                active: true,
-                label: (i18n.t("controllers.services.ui.CaseTabsSrv.details") || "Details"),
-                state: 'app.case.details'
-            },
-            'tasks': {
-                name: 'tasks',
-                active: false,
-                label: (i18n.t("controllers.services.ui.CaseTabsSrv.tasks") || "Tasks"),
-                state: 'app.case.tasks'
-            },
-            'observables': {
-                name: 'observables',
-                active: false,
-                label: ''(i18n.t("controllers.services.ui.CaseTabsSrv.observables") || "Observables"),
-                state: 'app.case.observables'
-            },
-            'procedures': {
-                name: 'procedures',
-                active: false,
-                label: 'TTPs'(i18n.t("controllers.services.ui.CaseTabsSrv.ttps") || "TTPs"),
-                state: 'app.case.procedures'
-            }
-        };
+		var tabs = {
+			'details': {
+				name: 'details',
+				active: true,
+				label: (i18n.t("controllers.services.ui.CaseTabsSrv.details") || "Details"),
+				state: 'app.case.details'
+			},
+			'tasks': {
+				name: 'tasks',
+				active: false,
+				label: (i18n.t("controllers.services.ui.CaseTabsSrv.tasks") || "Tasks"),
+				state: 'app.case.tasks'
+			},
+			'observables': {
+				name: 'observables',
+				active: false,
+				label: (i18n.t("controllers.services.ui.CaseTabsSrv.observables") || "Observables"),
+				state: 'app.case.observables'
+			},
+			'procedures': {
+				name: 'procedures',
+				active: false,
+				label: (i18n.t("controllers.services.ui.CaseTabsSrv.ttps") || "TTPs"),
+				state: 'app.case.procedures'
+			}
+		};
 
-        this.activeIndex = 0;
+		this.activeIndex = 0;
 
-        this.initTabs = function() {
-            angular.forEach(tabs, function(tab, key) {
-                if (tab.closable === true) {
-                    delete tabs[key];
-                }
-            });
-        };
+		this.initTabs = function() {
+			angular.forEach(tabs, function(tab, key) {
+				if (tab.closable === true) {
+					delete tabs[key];
+				}
+			});
+		};
 
-        this.getTabs = function() {
-            return tabs;
-        };
+		this.getTabs = function() {
+			return tabs;
+		};
 
-        this.getTab = function(name) {
-            return tabs[name];
-        };
+		this.getTab = function(name) {
+			return tabs[name];
+		};
 
-        this.activateTab = function(tab) {
-            angular.forEach(tabs, function(tab) {
-                tab.active = false;
-            });
+		this.activateTab = function(tab) {
+			angular.forEach(tabs, function(tab) {
+				tab.active = false;
+			});
 
-            if (tabs[tab]) {
-                tabs[tab].active = true;
-                this.activeIndex = Object.keys(tabs).indexOf(tab);
-            } else {
-                tabs.details.active = true;
-                this.activeIndex = 0;
-            }
-        };
+			if (tabs[tab]) {
+				tabs[tab].active = true;
+				this.activeIndex = Object.keys(tabs).indexOf(tab);
+			} else {
+				tabs.details.active = true;
+				this.activeIndex = 0;
+			}
+		};
 
-        this.addTab = function(tab, options) {
-            options.closable = true;
+		this.addTab = function(tab, options) {
+			options.closable = true;
 
-            tabs[tab] = options;
-            this.activeIndex = Object.keys(tabs).length - 1;
-        };
+			tabs[tab] = options;
+			this.activeIndex = Object.keys(tabs).length - 1;
+		};
 
-        this.removeTab = function(tab) {
-            var tabItem = tabs[tab];
+		this.removeTab = function(tab) {
+			var tabItem = tabs[tab];
 
-            if (!tabItem) {
-                return;
-            }
+			if (!tabItem) {
+				return;
+			}
 
-            var currentIsActive = tabItem.active;
+			var currentIsActive = tabItem.active;
 
-            delete tabs[tab];
+			delete tabs[tab];
 
-            if (currentIsActive) {
-                return true;
-            } else {
-                return false;
-            }
+			if (currentIsActive) {
+				return true;
+			} else {
+				return false;
+			}
 
-        };
+		};
 
-    });
+	});
 })();
